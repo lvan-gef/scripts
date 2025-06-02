9f7c4c4aab324d86daa86223a3d6fddf8b73494e
luuk <luukvangeffen@protonmail.com>
2025-06-03 01:06:41 +0200

    Update path handler

--- update_tools.sh
+++ update_tools.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash
 
-DIR="$HOME/build_dir/"
+DIR="$HOME/build_dir"
 
 # add checking if commands where succesfull
-cd "$DIR/fd"
+cd "$DIR/fd" || exit
 git pull --rebase
 cargo build --relase
 cargo test
 cargo install --path .
 
-cd "$DIR/ripgrep"
+cd "$DIR/ripgrep" || exit
 git pull --rebase
 cargo build --release --features 'pcre2'
 
-cd "$DIR/fzf"
+cd "$DIR/fzf" || exit
 git pull --rebase
 ./install
