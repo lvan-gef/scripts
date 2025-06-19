c6211ac653405073427ff338d286c115bdd767ac
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-06-19 10:24:46 +0200

    Feat: Add mapping for macOS

--- caps_to_esc.sh
+++ caps_to_esc.sh
@@ -1,16 +1,29 @@
 #! /usr/bin/env bash
 
-STATE=$(xmodmap -pke | grep " 66 = " | grep "Caps_Lock")
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    STATE=$(hidutil property --get "UserKeyMapping" | grep "{")
+    RESULT_CODE="$?"
 
-if [ -z "$STATE" ]; then
-    # esc is mapped to Caps_Lock
-    xmodmap -e "add Lock = Caps_Lock"
-    xmodmap -e "keycode 66 = Caps_Lock"
-else
-    # capslock is mapped to Caps_Lock
-    xmodmap -e "clear Lock"
-    xmodmap -e "keysym Caps_Lock = Escape"
-fi
+    if [[ $RESULT_CODE -eq 0 ]]; then
+        # esc is mapped to Caps_Lock
+        hidutil property --set '{"UserKeyMapping":[]}' > /dev/null
+    else
+        # capslock is mapped to Caps_Lock
+        hidutil property --set '{"UserKeyMapping":[ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000029} ]}' > /dev/null
+    fi
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    STATE=$(xmodmap -pke | grep " 66 = " | grep "Caps_Lock")
+
+    if [ -z "$STATE" ]; then
+        # esc is mapped to Caps_Lock
+        xmodmap -e "add Lock = Caps_Lock"
+        xmodmap -e "keycode 66 = Caps_Lock"
+    else
+        # capslock is mapped to Caps_Lock
+        xmodmap -e "clear Lock"
+        xmodmap -e "keysym Caps_Lock = Escape"
+    fi
+fi;
 
 # too update companion state
 if [[ $# -eq 1 ]]; then
