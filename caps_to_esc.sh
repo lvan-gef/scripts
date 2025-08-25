f6ab3d048720d2f1bc28105b164ecf81e58ca1d0
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-08-25 22:45:40 +0200

    Feat: post current state made easyer

--- caps_to_esc.sh
+++ caps_to_esc.sh
@@ -1,27 +1,29 @@
 #! /usr/bin/env bash
 
+NEW_STATE="Unkown"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     STATE=$(hidutil property --get "UserKeyMapping" | grep "{")
     RESULT_CODE="$?"
 
     if [[ $RESULT_CODE -eq 0 ]]; then
-        # esc is mapped to Caps_Lock
         hidutil property --set '{"UserKeyMapping":[]}' > /dev/null
+        NEW_STATE='Caps'
     else
-        # capslock is mapped to Caps_Lock
         hidutil property --set '{"UserKeyMapping":[ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000029} ]}' > /dev/null
+        NEW_STATE='Esc'
     fi
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     STATE=$(xmodmap -pke | grep " 66 = " | grep "Caps_Lock")
 
     if [ -z "$STATE" ]; then
-        # esc is mapped to Caps_Lock
         xmodmap -e "add Lock = Caps_Lock"
         xmodmap -e "keycode 66 = Caps_Lock"
+        NEW_STATE="Caps"
     else
-        # capslock is mapped to Caps_Lock
         xmodmap -e "clear Lock"
         xmodmap -e "keysym Caps_Lock = Escape"
+        NEW_STATE="Esc"
     fi
 fi;
 
@@ -29,7 +31,6 @@ fi;
 if [[ $# -eq 1 ]]; then
     case $1 in
         'u')
-            NEW_STATE=$(xmodmap -pke | grep " 66 = " | awk '{ print $4 } ' | tr '[:upper:]' '[:lower:]' | tr -s '_' ' ')
             curl -H 'Content-Type: application/json' -d "{\"text\": \"$NEW_STATE\"}" -X POST 'http://localhost:8888/api/location/1/0/3/style'
             ;;
         *)
