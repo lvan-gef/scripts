d2034c782a2ce938551b099aad46855654256c6f
Luuk van Geffen <lvan-gef@student.codam.nl>
2025-10-06 08:43:40 +0200

    Add state handler

--- caps_to_esc.sh
+++ caps_to_esc.sh
@@ -1,40 +1,62 @@
 #! /usr/bin/env bash
 
-NEW_STATE="Unkown"
+get_state() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        local state=$(hidutil property --get "UserKeyMapping" | grep "{")
+        local result_code="$?"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    STATE=$(hidutil property --get "UserKeyMapping" | grep "{")
-    RESULT_CODE="$?"
+        if [[ $result_code -eq 0 ]]; then
+            echo "Esc"
+        else
+            echo "Caps"
+        fi
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        local state=$(xmodmap -pke | grep " 66 = " | grep "Caps_Lock")
 
-    if [[ $RESULT_CODE -eq 0 ]]; then
-        hidutil property --set '{"UserKeyMapping":[]}' > /dev/null
-        NEW_STATE='Caps'
-    else
-        hidutil property --set '{"UserKeyMapping":[ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000029} ]}' > /dev/null
-        NEW_STATE='Esc'
+        if [ -z "$state" ]; then
+            echo "Esc"
+        else
+            echo "Caps"
+        fi
     fi
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    STATE=$(xmodmap -pke | grep " 66 = " | grep "Caps_Lock")
+}
 
-    if [ -z "$STATE" ]; then
-        xmodmap -e "add Lock = Caps_Lock"
-        xmodmap -e "keycode 66 = Caps_Lock"
-        NEW_STATE="Caps"
-    else
-        xmodmap -e "clear Lock"
-        xmodmap -e "keysym Caps_Lock = Escape"
-        NEW_STATE="Esc"
+new_state() {
+    local cur_state=$(get_state)
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        if [ "$cur_state" = "Caps" ]; then
+            hidutil property --set '{"UserKeyMapping":[ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x700000029} ]}' > /dev/null
+            echo "Esc"
+        else
+            hidutil property --set '{"UserKeyMapping":[]}' > /dev/null
+            echo "Caps"
+        fi
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [ "$cur_state" = "Caps" ]; then
+            xmodmap -e "clear Lock"
+            xmodmap -e "keysym Caps_Lock = Escape"
+            echo "Esc"
+        else
+            xmodmap -e "add Lock = Caps_Lock"
+            xmodmap -e "keycode 66 = Caps_Lock"
+            echo "Caps"
+        fi
     fi
-fi
+}
 
-# too update companion state
 if [[ $# -eq 1 ]]; then
     case $1 in
         'u')
-            curl -H 'Content-Type: application/json' -d "{\"text\": \"$NEW_STATE\"}" -X POST 'http://localhost:8888/api/location/1/0/3/style'
+            CUR_STATE=$(new_state)
+            curl -H 'Content-Type: application/json' -d "{\"text\": \"$CUR_STATE\"}" -X POST 'http://localhost:8888/api/location/1/0/3/style'
+            ;;
+        's')
+            CUR_STATE=$(get_state)
+            curl -H 'Content-Type: application/json' -d "{\"text\": \"$CUR_STATE\"}" -X POST 'http://localhost:8888/api/location/1/0/3/style'
             ;;
         *)
-            echo 'Expected "u" for update state'
+            echo 'Expected "u" for update state or "s" for current state'
             ;;
     esac
 fi
